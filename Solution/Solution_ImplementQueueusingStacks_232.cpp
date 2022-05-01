7f665c838528a7f22d9c3c34d819864de761bfc6
aerim-choi <80438964+aerim-choi@users.noreply.github.com>
2022-05-01 18:47:23 +0900

    Create Solution_ImplementQueueusingStacks_232.cpp

---
+++ Solution/Solution_ImplementQueueusingStacks_232.cpp
@@ -0,0 +1,34 @@
+class MyQueue {
+    stack<int> s;
+    stack<int> temp;
+public:
+    //Time Complexity:O(N)
+    void push(int x) {
+        while (!s.empty()) {
+            temp.push(s.top());
+            s.pop();
+        }
+        
+        s.push(x);
+        
+        while (!temp.empty()) {
+            s.push(temp.top());
+            temp.pop();
+        }
+
+    }
+    //Time Complexity:O(1)
+    int pop() {
+        int n =s.top();
+        s.pop();
+        return n;
+     }
+   //Time Complexity:O(1)
+    int peek() {
+        return s.top();
+    }
+    //Time Complexity:O(1)
+    bool empty() {
+        return s.empty();
+    }
+};
