9fdfd8cec9112d2334dc179db5ce0a815ccf629e
aerim-choi <80438964+aerim-choi@users.noreply.github.com>
2022-05-17 18:14:09 +0900

    Update Solution_FindAllAnagramsinaString_438.cpp

--- Solution/Solution_FindAllAnagramsinaString_438.cpp
+++ Solution/Solution_FindAllAnagramsinaString_438.cpp
@@ -41,3 +41,76 @@ int main() {
         cout << i << " ";
     }
 }
+
+//시간초과1
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        int pnum = 0; //문자열 p의 아스키 코드값의 세제곱합
+        int snum = 0; //슬라이싱 된 문자열 s의 아스키 코드값의 세제곱합 
+   
+        vector<int> ans;
+        if (s.length() < p.length())return ans;
+        //문자열 p의 아스키 코드값 세제곱의합 구하기
+        for (int i = 0; i < p.length(); i++) {
+            pnum += pow(((int)(p[i] - 'a')), 3);
+        }
+        
+        for (int i = 0; i <= s.length() - p.length(); i++) {
+            for (int j = i; j < i + p.length(); j++) {
+                snum += pow(((int)(s[j] - 'a')), 3);                 \
+            }
+
+            //아스키 코드값이 같다면
+            if (pnum == snum) {
+                ans.push_back(i); //인덱스 값 저장
+            }
+            snum = 0; //문자열 s의 아스키코드값의 세제곱합 초기화
+           
+        }
+        
+        return ans;
+    }
+    
+};
+
+int main() {
+    Solution s;
+    for (int i : s.findAnagrams("cbaebabacd", "abc")) {
+        cout << i << " ";
+    }
+}
+//시간초과2
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        string temp;
+   
+        vector<int> ans;
+        if (s.length() < p.length())return ans;
+     
+        
+        for (int i = 0; i <= s.length() - p.length(); i++) {
+            for (int j = i; j < i + p.length(); j++) {
+                temp += s[j];                \
+            }
+            sort(p.begin(),p.end());
+            sort(temp.begin(), temp.end());
+            if (temp == p) {
+                ans.push_back(i);
+            }
+            temp = "";
+            
+        }
+        
+        return ans;
+    }
+    
+};
