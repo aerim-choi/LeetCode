020dbcb0292dd54b58875579fa370b6de68f07c7
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2022-06-09 17:33:50 +0900

    Create Solution_ClimbingStairs_70.cpp
    동적프로그래밍
    O(N)/O(N)

---
+++ Solution/Solution_ClimbingStairs_70.cpp
@@ -0,0 +1,21 @@
+//동적프로그래밍
+//O(N)/O(N)
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[46];
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+          dp[i] = dp[i-1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
