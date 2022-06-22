87e07ceb740065dbfae48b75e87e519d3a43451f
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2022-06-22 22:01:41 +0900

    https://blog.naver.com/sswuaerim/222784632977
    greedy Algorithm(탐욕알고리즘)
    O(N)/O(1)

---
+++ Solution/Solution_991.cpp
@@ -0,0 +1,19 @@
+//greedy Algorithm
+//O(N)/O(1)
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int ans = 0;
+        while(target > startValue){
+            ans++;
+            if(target % 2==1){
+                target++;
+            }
+            else{
+                target/=2;
+            }
+        }
+        return ans + startValue - target;
+    }
+    
+};
